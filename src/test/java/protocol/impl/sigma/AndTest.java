bc89b5f6d144932df576143c52fba4391fee142a
Hartbook <franck.dary@etu.univ-amu.fr>
2016-09-27 20:48:48 +0200

    Added AndTest

---
+++ src/test/java/protocol/impl/sigma/AndTest.java
@@ -0,0 +1,50 @@
+package protocol.impl.sigma;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.math.BigInteger;
+import java.util.HashMap;
+import protocol.impl.sigma.And;
+import crypt.factories.ElGamalAsymKeyFactory;
+import model.entity.ElGamalKey;
+import protocol.impl.sigma.Receiver;
+import protocol.impl.sigma.ResEncrypt;
+import protocol.impl.sigma.ResponsesSchnorr;
+import protocol.impl.sigma.Sender;
+import protocol.impl.sigma.Trent;
+
+public class AndTest {
+	
+	@Test
+	public void test() {
+		ElGamalKey receiverKey = ElGamalAsymKeyFactory.create(false);
+		ElGamalKey senderKey = ElGamalAsymKeyFactory.create(false);
+		Receiver receiver = new Receiver();
+		Sender sender = new Sender(senderKey);
+
+		ElGamalKey trentKey = ElGamalAsymKeyFactory.create(false);
+		Trent trent = new Trent(trentKey);
+
+		String message = "Message";
+		byte[] buffer = message.getBytes();
+		ResEncrypt resEncrypt = sender.Encryption(buffer, trent.getKey());
+
+		Responses response = sender.SendResponseSchnorrFabric(sender.getKeys());
+
+		HashMap<Responses, ElGamalKey> rK = new HashMap<Responses, ElGamalKey>();
+		rK.put(response, senderKey);
+
+		And and = new And(receiver, rK, resEncrypt, response);
+
+		assertFalse(and.Verifies(false));
+		assertTrue(and.Verifies(true));
+
+		rK.clear();
+		rK.put(response, receiverKey);
+		and = new And(receiver, rK, resEncrypt, response);
+
+		assertFalse(and.Verifies(true));
+	}
+}
+
