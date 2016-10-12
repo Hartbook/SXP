01e5724570fee7c46a14ef41ec51226b0f919643
franck <franck.dary@etu.univ-amu.fr>
2016-10-12 14:43:40 +0200

    Started JettyRestServerTest

---
+++ src/test/java/rest/impl/JettyRestServerTest.java
@@ -0,0 +1,33 @@
+package rest.impl;
+
+import java.lang.*;
+import java.util.Random;
+import java.math.BigInteger;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import rest.api.RestServer;
+import rest.factories.RestServerFactory;
+import rest.impl.JettyRestServer;
+import network.factories.PeerFactory;
+import network.api.Peer;
+
+public class JettyRestServerTest {
+
+	RestServerFactory factory = new RestServerFactory();
+
+	@Test
+	public void test() {
+		int restPort = 8081;
+
+		RestServer restServer = RestServerFactory.createAndStartDefaultRestServer(restPort);
+		
+		try
+		{
+			Thread.sleep(2000); // On laisse le temps au choses de s'init
+		}
+		catch (InterruptedException e)
+		{}
+
+		//restServer.stop();
+	}
+}
